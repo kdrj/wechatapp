fb469a7a41e6e7b7f0f57e57c05d843d8e59b6a6
kadeer <1370049660@qq.com>
2018-12-12 15:21:26 +0800

    firstcommit

---
+++ platform-api/src/main/java/com/platform/api/ApiBmiController.java
@@ -0,0 +1,32 @@
+package com.platform.api;/**
+ * @author Administrator
+ * @create 2018-12-11 14:27
+ * @desc BMI指数计算
+ **/
+
+import com.platform.annotation.LoginUser;
+import com.platform.entity.UserVo;
+import com.platform.util.ApiBaseAction;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+
+ * @author Administrator
+
+ * @create 2018-12-11 14:27
+
+ * @desc BMI指数计算
+
+ **/
+@RestController
+@RequestMapping("/api/bmi")
+public class ApiBmiController extends ApiBaseAction {
+    @PostMapping ("getbmi")
+    public Object getbmi(@LoginUser UserVo loginUser){
+
+        return toResponsSuccess(18);
+    }
+}
---
+++ wx-mall/pages/index/index.js
@@ -0,0 +1,256 @@
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+const user = require('../../services/user.js');
+
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    newGoods: [],
+    hotGoods: [],
+    topics: [],
+    brands: [],
+    floorGoods: [],
+    banner: [],
+    channel: [],
+
+    // 抽奖
+    circleList: [],//圆点数组
+    awardList: [],//奖品数组
+    colorCircleFirst: '#FFDF2F',//圆点颜色1
+    colorCircleSecond: '#FE4D32',//圆点颜色2
+    colorAwardDefault: '#F5F0FC',//奖品默认颜色
+    colorAwardSelect: '#ffe400',//奖品选中颜色
+    indexSelect: 0,//被选中的奖品index
+    isRunning: false,//是否正在抽奖
+    imageAward: [
+      // '../../images/1.jpg',
+      // '../../images/2.jpg',
+      // '../../images/3.jpg',
+      // '../../images/4.jpg',
+      // '../../images/5.jpg',
+      // '../../images/6.jpg',
+      // '../../images/7.jpg',
+      // '../../images/8.jpg',
+    ],//奖品图片数组
+  },
+  onShareAppMessage: function () {
+    return {
+      title: 'NideShop',
+      desc: '微信小程序商城',
+      path: '/pages/index/index'
+    }
+  },onPullDownRefresh(){
+	  	// 增加下拉刷新数据的功能
+	    var self = this;
+	    this.getIndexData();
+ },
+  getIndexData: function () {
+    let that = this;
+    var data = new Object();
+    util.request(api.IndexUrlNewGoods).then(function (res) {
+      if (res.errno === 0) {
+        data.newGoods= res.data.newGoodsList
+      that.setData(data);
+      }
+    });
+    util.request(api.IndexUrlHotGoods).then(function (res) {
+      if (res.errno === 0) {
+        data.hotGoods = res.data.hotGoodsList
+      that.setData(data);
+      }
+    });
+    util.request(api.IndexUrlTopic).then(function (res) {
+      if (res.errno === 0) {
+        data.topics = res.data.topicList
+      that.setData(data);
+      }
+    });
+    util.request(api.IndexUrlBrand).then(function (res) {
+      if (res.errno === 0) {
+        data.brand = res.data.brandList
+      that.setData(data);
+      }
+    });
+    util.request(api.IndexUrlCategory).then(function (res) {
+      if (res.errno === 0) {
+        data.floorGoods = res.data.categoryList
+      that.setData(data);
+      }
+    });
+    util.request(api.IndexUrlBanner).then(function (res) {
+
+      if (res.errno === 0) {
+        data.banner = res.data.banner
+      that.setData(data);
+      }
+    });
+    util.request(api.IndexUrlChannel).then(function (res) {
+      if (res.errno === 0) {
+        data.channel = res.data.channel
+      that.setData(data);
+      }
+    });
+
+  },
+  onLoad: function (options) {
+    this.gamecenter();
+    // 地图开始
+    this.mapCtx = wx.createMapContext('myMap');//获取地图对象同canvas相似，获取后才能调用相应的方法
+    this.mapCtx.moveToLocation()//将当前位置移动到地图中心
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 37.4387984768,
+        longitude: 121.4334297180,
+      }, {
+          latitude: 37.4387984768,
+          longitude: 121.4334297180,
+      }]
+    })//缩放视野展示所有经纬度
+    //地图结束
+  },
+  //调用微信内置地图
+  gps:function(){
+    wx.openLocation({
+      latitude: 37.4387984768,
+      longitude: 121.4334297180
+    })
+  },
+
+  //游戏界面
+  gamecenter:function(){
+    this.getIndexData();
+    var _this = this;
+    //圆点设置
+    var leftCircle = 7.5;
+    var topCircle = 7.5;
+    var circleList = [];
+    for (var i = 0; i < 24; i++) {
+      if (i == 0) {
+        topCircle = 15;
+        leftCircle = 15;
+      } else if (i < 6) {
+        topCircle = 7.5;
+        leftCircle = leftCircle + 102.5;
+      } else if (i == 6) {
+        topCircle = 15
+        leftCircle = 620;
+      } else if (i < 12) {
+        topCircle = topCircle + 94;
+        leftCircle = 620;
+      } else if (i == 12) {
+        topCircle = 565;
+        leftCircle = 620;
+      } else if (i < 18) {
+        topCircle = 570;
+        leftCircle = leftCircle - 102.5;
+      } else if (i == 18) {
+        topCircle = 565;
+        leftCircle = 15;
+      } else if (i < 24) {
+        topCircle = topCircle - 94;
+        leftCircle = 7.5;
+      } else {
+        return
+      }
+      circleList.push({ topCircle: topCircle, leftCircle: leftCircle });
+    }
+    this.setData({
+      circleList: circleList
+    })
+    //圆点闪烁
+    setInterval(function () {
+      if (_this.data.colorCircleFirst == '#FFDF2F') {
+        _this.setData({
+          colorCircleFirst: '#FE4D32',
+          colorCircleSecond: '#FFDF2F',
+        })
+      } else {
+        _this.setData({
+          colorCircleFirst: '#FFDF2F',
+          colorCircleSecond: '#FE4D32',
+        })
+      }
+    }, 500)
+    //奖品item设置
+    var awardList = [];
+    //间距,怎么顺眼怎么设置吧.
+    var topAward = 25;
+    var leftAward = 25;
+    for (var j = 0; j < 8; j++) {
+      if (j == 0) {
+        topAward = 25;
+        leftAward = 25;
+      } else if (j < 3) {
+        topAward = topAward;
+        //166.6666是宽.15是间距.下同
+        leftAward = leftAward + 166.6666 + 15;
+      } else if (j < 5) {
+        leftAward = leftAward;
+        //150是高,15是间距,下同
+        topAward = topAward + 150 + 15;
+      } else if (j < 7) {
+        leftAward = leftAward - 166.6666 - 15;
+        topAward = topAward;
+      } else if (j < 8) {
+        leftAward = leftAward;
+        topAward = topAward - 150 - 15;
+      }
+      var imageAward = this.data.imageAward[j];
+      awardList.push({ topAward: topAward, leftAward: leftAward, imageAward: imageAward });
+    }
+    this.setData({
+      awardList: awardList
+    })
+  },
+  //开始游戏
+  startGame: function () {
+    if (this.data.isRunning) return
+    this.setData({
+      isRunning: true
+    })
+    var _this = this;
+    var indexSelect = 0
+    var i = 0;
+    var timer = setInterval(function () {
+      indexSelect++;
+      //这里我只是简单粗暴用y=30*x+200函数做的处理.可根据自己的需求改变转盘速度
+      i += 30;
+      if (i > 1000) {
+        //去除循环
+        clearInterval(timer)
+        //获奖提示
+
+        wx.showModal({
+          title: '恭喜您',
+          content: '获得了第' + (_this.data.indexSelect + 1) + "个优惠券",
+          showCancel: false,//去掉取消按钮
+          success: function (res) {
+            if (res.confirm) {
+              _this.setData({
+                isRunning: false
+              })
+            }
+          }
+        })
+      }
+      indexSelect = indexSelect % 8;
+      _this.setData({
+        indexSelect: indexSelect
+      })
+    }, (200 + i))
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  },
+})
